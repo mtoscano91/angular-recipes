410778a1ec89c57a2249fa2857dca19c8697606a
Manu <manueltoscano91@hotmail.com>
2020-07-16 16:34:21 +0200

    added firebase backend

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,20 +7,28 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Test Recipe',
-      'This is simply a test',
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg',
-      [new Ingredient('Meat', 3), new Ingredient('Banana', 4)]
-    ),
-    new Recipe(
-      'Another Recipe',
-      'This is simply a test',
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg',
-      [new Ingredient('Bread', 2), new Ingredient('Meat', 4)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Test Recipe',
+  //     'This is simply a test',
+  //     'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg',
+  //     [new Ingredient('Meat', 3), new Ingredient('Banana', 4)]
+  //   ),
+  //   new Recipe(
+  //     'Another Recipe',
+  //     'This is simply a test',
+  //     'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg',
+  //     [new Ingredient('Bread', 2), new Ingredient('Meat', 4)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
   getRecipe(index: number) {
     return this.recipes[index];
   }
